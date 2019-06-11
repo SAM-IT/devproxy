f834add08e0f7b669b6d64b4edbc2675d597fc4f
Sam Mousa <sam@mousa.nl>
2019-06-11 11:23:35 +0200

    Automated proxy

---
+++ docker/create-nginx-config.php
@@ -0,0 +1,122 @@
+#!/usr/bin/env php
+<?php
+
+function get_eligible_containers(): array
+{
+    $ids = [];
+    exec("docker ps -f label='com.awesam.proxy.domains' -q", $ids);
+    return $ids;
+}
+
+function get_domains(string $container): array
+{
+    return explode(',', trim(shell_exec("docker inspect --format '{{ index .Config.Labels \"com.awesam.proxy.domains\" }}' $container")));
+}
+
+function get_ip(string $container): string
+{
+    return trim(shell_exec("docker inspect --format '{{ .NetworkSettings.Networks.devproxy.IPAddress }}' $container"));
+}
+
+function get_name(string $container): string
+{
+    return trim(shell_exec("docker inspect --format '{{ .Name }}' '$container'"), "\n/");
+}
+
+function create_server_block(
+    string $container
+): void {
+
+    $serverName = implode(' ', array_map(function($hostname) {
+        return "$hostname.*";
+    }, get_domains($container)));
+    $ip = get_ip($container);
+    $name = get_name($container);
+    $template = <<<NGINX
+server {
+  listen 443 ssl;
+  listen 4430 ssl;
+  ssl_certificate /tmp/certs/$name.crt;
+  ssl_certificate_key /config/private.key;
+  server_name $serverName;
+  client_max_body_size 20m;
+  location / {
+      proxy_set_header X-Forwarded-For \$remote_addr;
+      proxy_buffer_size 128k;
+      proxy_buffers 4 256k;
+      proxy_busy_buffers_size 256k;
+      proxy_set_header Host \$http_host;
+      proxy_set_header X-Forwarded-Port \$server_port;
+      proxy_set_header X-Forwarded-Proto "https";
+      proxy_pass http://$ip;
+  }
+}
+
+NGINX;
+    @mkdir("/tmp/nginxblocks", "0777", true);
+    file_put_contents("/tmp/nginxblocks/$name.conf", $template);
+}
+
+function create_ssl_certificate(string $name, array $domains)
+{
+    $handle = tmpfile();
+    $config = <<<OPENSSL
+[req]
+req_extensions = req_ext
+distinguished_name = dn
+
+[req_ext]
+subjectAltName=@alt_names
+
+[dn]
+C = NL
+CN = Example
+
+[alt_names]
+
+OPENSSL;
+
+    $i = 1;
+    foreach($domains as $domain) {
+        $config .= "DNS.$i=$domain.test\n";
+        $i++;
+        $config .= "DNS.$i=*.$domain.test\n";
+        $i++;
+    }
+    fwrite($handle, $config);
+    $configFile = stream_get_meta_data($handle)['uri'];
+    echo file_get_contents($configFile);
+
+    @mkdir('/tmp/certs', '0777', true);
+
+    $cmd = "openssl req -new -key /config/private.key -subj '/C/NL/CN=$name' -config $configFile | "
+        . " openssl x509 -req -CA /config/ca.crt -extfile $configFile -extensions req_ext "
+        . "-CAkey /config/private.key -CAcreateserial -days 10 -out /tmp/certs/$name.crt"
+        ;
+
+    passthru($cmd);
+    
+}
+
+
+echo "Checking containers\n";
+// Clear old blocks.
+passthru('rm -rf /tmp/nginxblocks');
+foreach(get_eligible_containers() as $id) {
+    $name = get_name($id);
+    echo "Creating config for $name ($id)\n";
+    $ip = get_ip($id);
+    echo "Found IP: $ip\n";
+    $domains = get_domains($id);
+    echo "Creating SSL config\n";
+    create_ssl_certificate($name, $domains);
+    create_server_block($id);
+//    foreach( as $domain) {
+//        echo "Domain: $domain\n";
+//    }
+}
+
+
+
+// Reload nginx config
+passthru('nginx -s reload');
---
+++ docker/entrypoint.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+echo Starting stuff
+mkdir -p /config/certs
+# Create private key, used for CA as well as all certificates.
+cd /config
+if [ ! -f openssl.cnf ]; then
+  cp /etc/ssl/openssl.cnf /config
+fi
+if [ ! -f private.key ]; then
+  echo "Creating new private key";
+  openssl genrsa -out private.key 4096
+fi
+
+if [ ! -f ca.crt ]; then
+  echo "Creating new CA certificate";
+  openssl req -x509 -new -nodes -key private.key -sha256 -days 1024 -out ca.crt -subj "/C=NL/CN=DevProxy"
+fi
+
+configFile=$(mktemp /tmp/csr.XXXXXX)
+config="
+[req]
+req_extensions = req_ext
+distinguished_name = dn
+
+[req_ext]
+subjectAltName=@alt_names
+
+[dn]
+C = NL
+CN = Example
+[alt_names]
+DNS.1=devproxy.test
+DNS.2=*.test
+"
+
+#cat /etc/ssl/openssl.cnf > $configFile
+echo "$config" > $configFile
+echo Creating sample cert
+openssl req \
+  -new \
+  -key private.key \
+  -subj "/C/NL/CN=devproxy.test" \
+  -config $configFile \
+  | openssl x509 -req -CA ca.crt -extfile $configFile -extensions req_ext -CAkey private.key -CAcreateserial -days 10 -out certs/devproxy.crt
+
+ openssl x509 -in certs/devproxy.crt -text -noout | grep DNS
+
+rm $configFile
+nginx &
+sleep 5
+/create-nginx-config.php
+echo "Waiting for docker events"
+docker events --filter event=start --format=containerstart &
+docker events --filter event=start --format=containerstart | xargs -t -n 1 /create-nginx-config.php
